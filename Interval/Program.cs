5defbdf68738d50c14a39741eae34de3f666a1bb
IsaMelendezT <157085293+IsaMelendezT@users.noreply.github.com>
2024-02-11 10:43:11 -0500

    Initial Code

--- Interval/Program.cs
+++ Interval/Program.cs
@@ -10,8 +10,19 @@ namespace Interval
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a real number: ");
+            double x = Double.Parse(Console.ReadLine());
 
+            bool belong = ((x >= -10) && (x <= -2)) || ((x >= 2) && (x < 3)) || ((x > 0) && (x <= 1));
 
+            if (belong)
+            {
+                Console.WriteLine("{0} x belongs to I ...", x);
+            }
+            else
+            {
+                Console.WriteLine("{0} x does not belong to I ...", x);
+            }
 
         }
     }
