d0efdbad2e63285e6e7c3bcc06d85d3a139a00d0
IsaMelendezT <157085293+IsaMelendezT@users.noreply.github.com>
2024-02-11 10:44:56 -0500

    Adding Documentation tot the code

--- Interval/Program.cs
+++ Interval/Program.cs
@@ -10,11 +10,14 @@ namespace Interval
     {
         static void Main(string[] args)
         {
+            // Asking input from the user
             Console.WriteLine("Please enter a real number: ");
             double x = Double.Parse(Console.ReadLine());
 
+            // Declaring boolean variable to see if the user input belongs to the set
             bool belong = ((x >= -10) && (x <= -2)) || ((x >= 2) && (x < 3)) || ((x > 0) && (x <= 1));
 
+            // Using if-else to determine which answer to prompt according to the boolean variable
             if (belong)
             {
                 Console.WriteLine("{0} x belongs to I ...", x);
